b322035ceed6cef17452663227cdde5a8c103ae6
martinRenou <martin.renou@gmail.com>
2021-05-27 10:47:02 +0200

    Add jupyterlab dependency

--- setup.py
+++ setup.py
@@ -43,7 +43,8 @@ setup_args = dict(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
-        "jupyter_server>=1.6,<2"
+        "jupyter_server>=1.6,<2",
+        "jupyterlab==3.1.0-alpha.10"
     ],
     zip_safe=False,
     include_package_data=True,
