3b91a144918fe6daafdbd0ea99b75c1ede6661e1
martinRenou <martin.renou@gmail.com>
2021-08-17 16:18:54 +0200

    First test

--- ui-tests/tests/jupyterlab-unfold.spec.ts
+++ ui-tests/tests/jupyterlab-unfold.spec.ts
@@ -2,13 +2,8 @@ import { test, expect } from '@playwright/test';
 
 const TARGET_URL = process.env.TARGET_URL ?? 'http://localhost:8888';
 
-test('should emit console message', async ({ page }) => {
-  // const logs: string[] = [];
 
-  // page.on('console', message => {
-  //   logs.push(message.text());
-  // });
-
+test('should unfold', async ({ page }) => {
   await page.goto(`${TARGET_URL}/lab`);
   await page.waitForSelector('#jupyterlab-splash', { state: 'detached' });
   await page.waitForSelector('div[role="main"] >> text=Launcher');
@@ -20,9 +15,8 @@ test('should emit console message', async ({ page }) => {
   // });
 
   expect(
-    await page.locator('div[role="main"]').screenshot()
-    // await page.locator('div[role="main"] >> text=light_single.png').screenshot()
-  ).toMatchSnapshot('test-image.png');
+    await page.locator('.jp-DirListing-content').screenshot()
+  ).toMatchSnapshot('first-render.png');
 
   // expect(
   //   logs.filter(s => s.startsWith('the JupyterLab main application'))
