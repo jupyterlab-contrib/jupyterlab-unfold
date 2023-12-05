c4a0a0150a652bc502153270c1bf82664021770d
HaudinFlorence <haudin.florence@gmail.com>
2023-12-05 10:57:11 +0100

    Implement the OpenDrivesDialog command to open a dialog when clicking on the drive button in the filebrowser toolbar. Add the necessary components to place inside the dialog to be able to pick drive by name or url and display the selected drives list in a table.

--- src/index.ts
+++ src/index.ts
@@ -27,11 +27,11 @@ import { DriveIcon } from './icons';
 
 import { addJupyterLabThemeChangeListener } from '@jupyter/web-components';
 
-//import { Dialog, showDialog } from '@jupyterlab/apputils';
+import { Dialog, showDialog } from '@jupyterlab/apputils';
 
-//import { Drive } from './contents';
+import { Drive } from './contents';
 
-//import { DriveListModel, DriveListView } from './drivelistmanager';
+import { DriveListModel, DriveListView } from './drivelistmanager';
 
 const SETTINGS_ID = 'jupyterlab-unfold:jupyterlab-unfold-settings';
 
@@ -130,6 +130,44 @@ export async function activateAddDrivesPlugin(
   const trans = translator.load('jupyterlab_unfold');
   console.log('AddDrives plugin is activated!');
   const { commands } = app;
+  const cocoDrive = new Drive(app.docRegistry);
+  cocoDrive.name = 'coconutDrive';
+  cocoDrive.baseUrl = '/coconut/url';
+  cocoDrive.region = '';
+  cocoDrive.status = 'active';
+  cocoDrive.provider = '';
+  const peachDrive = new Drive(app.docRegistry);
+  peachDrive.baseUrl = '/peach/url';
+  peachDrive.name = 'peachDrive';
+  const mangoDrive = new Drive(app.docRegistry);
+  mangoDrive.baseUrl = '/mango/url';
+  mangoDrive.name = 'mangoDrive';
+  const kiwiDrive = new Drive(app.docRegistry);
+  kiwiDrive.baseUrl = '/kiwi/url';
+  kiwiDrive.name = 'kiwiDrive';
+  const pearDrive = new Drive(app.docRegistry);
+  pearDrive.baseUrl = '/pear/url';
+  pearDrive.name = 'pearDrive';
+  const customDrive = new Drive(app.docRegistry);
+  customDrive.baseUrl = '/customDrive/url';
+  const tomatoDrive = new Drive(app.docRegistry);
+  tomatoDrive.baseUrl = '/tomato/url';
+  tomatoDrive.name = 'tomatoDrive';
+  const avocadoDrive = new Drive(app.docRegistry);
+  avocadoDrive.baseUrl = '/avocado/url';
+  avocadoDrive.name = 'avocadoDrive';
+
+  const selectedList1: Drive[] = [];
+  const availableList1: Drive[] = [
+    avocadoDrive,
+    cocoDrive,
+    customDrive,
+    kiwiDrive,
+    mangoDrive,
+    peachDrive,
+    pearDrive,
+    tomatoDrive
+  ];
 
   const model = new FilterFileTreeBrowserModel({
     translator: translator,
@@ -166,10 +204,47 @@ export async function activateAddDrivesPlugin(
   );
 
   addJupyterLabThemeChangeListener();
+  const selectedDrivesModelMap = new Map<Drive[], DriveListModel>();
+  let selectedDrives: Drive[] = selectedList1;
+  const availableDrives: Drive[] = availableList1;
+  let driveListModel = selectedDrivesModelMap.get(selectedDrives);
+
+  function addDriveContentsToPanel() {
+    console.log('A drive is added to the Panel');
+  }
 
   commands.addCommand(CommandIDs.openDrivesDialog, {
     execute: async args => {
-      console.log('You have clicked on D button');
+      if (!driveListModel) {
+        driveListModel = new DriveListModel(availableDrives, selectedDrives);
+        selectedDrivesModelMap.set(selectedDrives, driveListModel);
+      } else {
+        selectedDrives = driveListModel.selectedDrives;
+        selectedDrivesModelMap.set(selectedDrives, driveListModel);
+      }
+      async function onDriveAdded(selectedDrives: Drive[]) {
+        if (driveListModel) {
+          const response = driveListModel.sendConnectionRequest(selectedDrives);
+          if ((await response) === true) {
+            addDriveContentsToPanel();
+          } else {
+            console.warn('Connection with the drive was not possible');
+          }
+        }
+      }
+
+      if (driveListModel) {
+        showDialog({
+          body: new DriveListView(driveListModel, app.docRegistry),
+          buttons: [Dialog.cancelButton()]
+        });
+      }
+
+      driveListModel.stateChanged.connect(async () => {
+        if (driveListModel) {
+          onDriveAdded(driveListModel.selectedDrives);
+        }
+      });
     },
 
     icon: DriveIcon.bindprops({ stylesheet: 'menuItem' }),
